9f3400ae9401f521f63cb969b46ad695aecbe903
Aleksei Degtiarev <delexa0@gmail.com>
2018-02-20 23:54:55 +0100

    Added connection to single device

--- DataCollector/Controller/CollectingDataVC.swift
+++ DataCollector/Controller/CollectingDataVC.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import CoreBluetooth
 
-class CollectingDataVC: UIViewController {
+class CollectingDataVC: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate  {
     
+    // Controlls outlets
     @IBOutlet weak var currentNumberOfSensorsLabel: UILabel!
     @IBOutlet weak var numberOfSensorsSlider: UISlider!
     @IBOutlet weak var sensorsStatusLabel: UILabel!
@@ -23,19 +25,380 @@ class CollectingDataVC: UIViewController {
     @IBOutlet weak var stopButton: UIButton!
     
     
+    // Define our scanning interval times
+    var keepScanning = false
+    let timerPauseInterval:TimeInterval = 10.0
+    let timerScanInterval:TimeInterval = 2.0
+
+    
+    // Core Bluetooth properties
+    var centralManager:CBCentralManager!
+    var sensorTag:CBPeripheral?
+    var temperatureCharacteristic:CBCharacteristic?
+    var humidityCharacteristic:CBCharacteristic?
+    
+    // This could be simplified to "SensorTag" and check if it's a substring.
+    // (Probably a good idea to do that if you're using a different model of
+    // the SensorTag, or if you don't know what model it is...)
+    let sensorTagName = "CC2650 SensorTag"
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Update start current value
+        sensorsChangedNumber(numberOfSensorsSlider)
+        periodChangedNumber(periodSlider)
+        
+        
+        
+        // Create our CBCentral Manager
+        // delegate: The delegate that will receive central role events. Typically self.
+        // queue:    The dispatch queue to use to dispatch the central role events.
+        //           If the value is nil, the central manager dispatches central role events using the main queue.
+        centralManager = CBCentralManager(delegate: self, queue: nil)
+        
+        // configure initial UI
+        sensorsStatusLabel.text = "Searching"
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // MARK: - CBCentralManagerDelegate methods
+    
+    // Invoked when the central manager’s state is updated.
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        var showAlert = true
+        var message = ""
+        
+        switch central.state {
+        case .poweredOff:
+            message = "Bluetooth on this device is currently powered off."
+        case .unsupported:
+            message = "This device does not support Bluetooth Low Energy."
+        case .unauthorized:
+            message = "This app is not authorized to use Bluetooth Low Energy."
+        case .resetting:
+            message = "The BLE Manager is resetting; a state update is pending."
+        case .unknown:
+            message = "The state of the BLE Manager is unknown."
+        case .poweredOn:
+            showAlert = false
+            message = "Bluetooth LE is turned on and ready for communication."
+            
+            print(message)
+            keepScanning = true
+            _ = Timer(timeInterval: timerScanInterval, target: self, selector: #selector(pauseScan), userInfo: nil, repeats: false)
+            
+            // Initiate Scan for Peripherals
+            //Option 1: Scan for all devices
+            centralManager.scanForPeripherals(withServices: nil, options: nil)
+            
+            // Option 2: Scan for devices that have the service you're interested in...
+            //let sensorTagAdvertisingUUID = CBUUID(string: Device.SensorTagAdvertisingUUID)
+            //print("Scanning for SensorTag adverstising with UUID: \(sensorTagAdvertisingUUID)")
+            //centralManager.scanForPeripheralsWithServices([sensorTagAdvertisingUUID], options: nil)
+            
+        }
+        
+        if showAlert {
+            let alertController = UIAlertController(title: "Central Manager State", message: message, preferredStyle: UIAlertControllerStyle.alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertController.addAction(okAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+    }
+    
+    /*
+     Invoked when the central manager discovers a peripheral while scanning.
+     
+     The advertisement data can be accessed through the keys listed in Advertisement Data Retrieval Keys.
+     You must retain a local copy of the peripheral if any command is to be performed on it.
+     In use cases where it makes sense for your app to automatically connect to a peripheral that is
+     located within a certain range, you can use RSSI data to determine the proximity of a discovered
+     peripheral device.
+     
+     central - The central manager providing the update.
+     peripheral - The discovered peripheral.
+     advertisementData - A dictionary containing any advertisement data.
+     RSSI - The current received signal strength indicator (RSSI) of the peripheral, in decibels.
+     
+     */
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        print("centralManager didDiscoverPeripheral - CBAdvertisementDataLocalNameKey is \"\(CBAdvertisementDataLocalNameKey)\"")
+        
+        // Retrieve the peripheral name from the advertisement data using the "kCBAdvDataLocalName" key
+        if let peripheralName = advertisementData[CBAdvertisementDataLocalNameKey] as? String {
+            print("NEXT PERIPHERAL NAME: \(peripheralName)")
+            print("NEXT PERIPHERAL UUID: \(peripheral.identifier.uuidString)")
+            
+            if peripheralName == sensorTagName {
+                print("SENSOR TAG FOUND! ADDING NOW!!!")
+                // to save power, stop scanning for other devices
+                keepScanning = false
+//                disconnectButton.isEnabled = true
+                
+                // save a reference to the sensor tag
+                sensorTag = peripheral
+                sensorTag!.delegate = self
+                
+                // Request a connection to the peripheral
+                centralManager.connect(sensorTag!, options: nil)
+            }
+        }
+    }
+    
+    /*
+     Invoked when a connection is successfully created with a peripheral.
+     
+     This method is invoked when a call to connectPeripheral:options: is successful.
+     You typically implement this method to set the peripheral’s delegate and to discover its services.
+     */
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        print("**** SUCCESSFULLY CONNECTED TO SENSOR TAG!!!")
+        
+//        temperatureLabel.font = UIFont(name: temperatureLabelFontName, size: temperatureLabelFontSizeMessage)
+//        temperatureLabel.text = "Connected"
+        
+        // Now that we've successfully connected to the SensorTag, let's discover the services.
+        // - NOTE:  we pass nil here to request ALL services be discovered.
+        //          If there was a subset of services we were interested in, we could pass the UUIDs here.
+        //          Doing so saves battery life and saves time.
+        peripheral.discoverServices(nil)
     }
     
     
+    /*
+     Invoked when the central manager fails to create a connection with a peripheral.
+     
+     This method is invoked when a connection initiated via the connectPeripheral:options: method fails to complete.
+     Because connection attempts do not time out, a failed connection usually indicates a transient issue,
+     in which case you may attempt to connect to the peripheral again.
+     */
+    func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
+        print("**** CONNECTION TO SENSOR TAG FAILED!!!")
+    }
+    
+    
+    /*
+     Invoked when an existing connection with a peripheral is torn down.
+     
+     This method is invoked when a peripheral connected via the connectPeripheral:options: method is disconnected.
+     If the disconnection was not initiated by cancelPeripheralConnection:, the cause is detailed in error.
+     After this method is called, no more methods are invoked on the peripheral device’s CBPeripheralDelegate object.
+     
+     Note that when a peripheral is disconnected, all of its services, characteristics, and characteristic descriptors are invalidated.
+     */
+    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
+        print("**** DISCONNECTED FROM SENSOR TAG!!!")
+//        lastTemperature = 0
+//        updateBackgroundImageForTemperature(lastTemperature)
+//        circleView.isHidden = true
+//        temperatureLabel.font = UIFont(name: temperatureLabelFontName, size: temperatureLabelFontSizeMessage)
+//        temperatureLabel.text = "Tap to search"
+//        humidityLabel.text = ""
+//        humidityLabel.isHidden = true
+        if error != nil {
+            print("****** DISCONNECTION DETAILS: \(error!.localizedDescription)")
+        }
+        sensorTag = nil
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //MARK: - CBPeripheralDelegate methods
+    
+    /*
+     Invoked when you discover the peripheral’s available services.
+     
+     This method is invoked when your app calls the discoverServices: method.
+     If the services of the peripheral are successfully discovered, you can access them
+     through the peripheral’s services property.
+     
+     If successful, the error parameter is nil.
+     If unsuccessful, the error parameter returns the cause of the failure.
+     */
+    // When the specified services are discovered, the peripheral calls the peripheral:didDiscoverServices: method of its delegate object.
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        if error != nil {
+            print("ERROR DISCOVERING SERVICES: \(error?.localizedDescription)")
+            return
+        }
+        
+        // Core Bluetooth creates an array of CBService objects —- one for each service that is discovered on the peripheral.
+        if let services = peripheral.services {
+            for service in services {
+                print("Discovered service \(service)")
+                // If we found either the temperature or the humidity service, discover the characteristics for those services.
+                if (service.uuid == CBUUID(string: Device.TemperatureServiceUUID)) ||
+                    (service.uuid == CBUUID(string: Device.HumidityServiceUUID)) {
+                    peripheral.discoverCharacteristics(nil, for: service)
+                }
+            }
+        }
+    }
+    
+    
+    /*
+     Invoked when you discover the characteristics of a specified service.
+     
+     If the characteristics of the specified service are successfully discovered, you can access
+     them through the service's characteristics property.
+     
+     If successful, the error parameter is nil.
+     If unsuccessful, the error parameter returns the cause of the failure.
+     */
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        if error != nil {
+            print("ERROR DISCOVERING CHARACTERISTICS: \(error?.localizedDescription)")
+            return
+        }
+        
+        if let characteristics = service.characteristics {
+            let enableValue:UInt8 = 1
+            let enableBytes = Data(bytes: [enableValue])
+            
+            
+            for characteristic in characteristics {
+                // Temperature Data Characteristic
+                if characteristic.uuid == CBUUID(string: Device.TemperatureDataUUID) {
+                    // Enable the IR Temperature Sensor notifications
+                    temperatureCharacteristic = characteristic
+                    sensorTag?.setNotifyValue(true, for: characteristic)
+                }
+                
+                // Temperature Configuration Characteristic
+                if characteristic.uuid == CBUUID(string: Device.TemperatureConfig) {
+                    // Enable IR Temperature Sensor
+                    sensorTag?.writeValue(enableBytes, for: characteristic, type: .withResponse)
+                }
+                
+                if characteristic.uuid == CBUUID(string: Device.HumidityDataUUID) {
+                    // Enable Humidity Sensor notifications
+                    humidityCharacteristic = characteristic
+                    sensorTag?.setNotifyValue(true, for: characteristic)
+                }
+                
+                if characteristic.uuid == CBUUID(string: Device.HumidityConfig) {
+                    // Enable Humidity Temperature Sensor
+                    sensorTag?.writeValue(enableBytes, for: characteristic, type: .withResponse)
+                }
+            }
+        }
+    }
+    
+    
+    /*
+     Invoked when you retrieve a specified characteristic’s value,
+     or when the peripheral device notifies your app that the characteristic’s value has changed.
+     
+     This method is invoked when your app calls the readValueForCharacteristic: method,
+     or when the peripheral notifies your app that the value of the characteristic for
+     which notifications and indications are enabled has changed.
+     
+     If successful, the error parameter is nil.
+     If unsuccessful, the error parameter returns the cause of the failure.
+     */
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+        if error != nil {
+            print("ERROR ON UPDATING VALUE FOR CHARACTERISTIC: \(characteristic) - \(error?.localizedDescription)")
+            return
+        }
+        
+        // extract the data from the characteristic's value property and display the value based on the characteristic type
+        if let dataBytes = characteristic.value {
+            if characteristic.uuid == CBUUID(string: Device.TemperatureDataUUID) {
+//                displayTemperature(dataBytes)
+            } else if characteristic.uuid == CBUUID(string: Device.HumidityDataUUID) {
+//                displayHumidity(dataBytes)
+            }
+        }
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // MARK: - Bluetooth scanning
+    
+    @objc func pauseScan() {
+        // Scanning uses up battery on phone, so pause the scan process for the designated interval.
+        print("*** PAUSING SCAN...")
+        _ = Timer(timeInterval: timerPauseInterval, target: self, selector: #selector(resumeScan), userInfo: nil, repeats: false)
+        centralManager.stopScan()
+//        disconnectButton.isEnabled = true
+    }
+    
+    @objc func resumeScan() {
+        if keepScanning {
+            // Start scanning again...
+            print("*** RESUMING SCAN!")
+//            disconnectButton.isEnabled = false
+            sensorsStatusLabel.text = "Searching"
+            _ = Timer(timeInterval: timerScanInterval, target: self, selector: #selector(pauseScan), userInfo: nil, repeats: false)
+            centralManager.scanForPeripherals(withServices: nil, options: nil)
+        } else {
+//            disconnectButton.isEnabled = true
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // MARK - Update UI
     
     @IBAction func sensorsChangedNumber(_ sender: UISlider) {
         sender.setValue(sender.value.rounded(.down), animated: true)
---
+++ DataCollector/Model/Device.swift
@@ -0,0 +1,50 @@
+//
+//  Device.swift
+//  DataCollector
+//
+//  Created by Aleksei Degtiarev on 19/02/2018.
+//  Copyright © 2018 Aleksei Degtiarev. All rights reserved.
+//
+
+import Foundation
+
+
+//------------------------------------------------------------------------
+// Information about Texas Instruments SensorTag UUIDs can be found at:
+// http://processors.wiki.ti.com/index.php/SensorTag_User_Guide#Sensors
+//------------------------------------------------------------------------
+// From the TI documentation:
+//  The TI Base 128-bit UUID is: F0000000-0451-4000-B000-000000000000.
+//
+//  All sensor services use 128-bit UUIDs, but for practical reasons only
+//  the 16-bit part is listed in this document.
+//
+//  It is embedded in the 128-bit UUID as shown by example below.
+//
+//          Base 128-bit UUID:  F0000000-0451-4000-B000-000000000000
+//          "0xAA01" maps as:   F000AA01-0451-4000-B000-000000000000
+//                                  ^--^
+//------------------------------------------------------------------------
+
+struct Device {
+    
+    static let SensorTagAdvertisingUUID = "AA10"
+    
+    static let TemperatureServiceUUID = "F000AA00-0451-4000-B000-000000000000"
+    static let TemperatureDataUUID = "F000AA01-0451-4000-B000-000000000000"
+    static let TemperatureConfig = "F000AA02-0451-4000-B000-000000000000"
+
+    static let HumidityServiceUUID = "F000AA20-0451-4000-B000-000000000000"
+    static let HumidityDataUUID = "F000AA21-0451-4000-B000-000000000000"
+    static let HumidityConfig = "F000AA22-0451-4000-B000-000000000000"
+    
+    static let SensorDataIndexTempInfrared = 0
+    static let SensorDataIndexTempAmbient = 1
+    static let SensorDataIndexHumidityTemp = 0
+    static let SensorDataIndexHumidity = 1
+    
+    static let MovementServiceUUID = "F000AA80-0451-4000-B000-000000000000"
+    static let MovementDataUUID = "F000AA81-0451-4000-B000-000000000000"
+    static let MovementConfig = "F000AA82-0451-4000-B000-000000000000"
+    static let MovementPeriod = "F000AA83-0451-4000-B000-000000000000"
+}
