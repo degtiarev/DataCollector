8a12579152c4327e565f9df12aa41125a8521189
Aleksei Degtiarev <delexa0@gmail.com>
2018-03-01 05:48:31 +0100

    Fully redesigned UI

---
+++ DataCollector/Controller/SettingsTableVC.swift
@@ -0,0 +1,23 @@
+//
+//  SettingsTableVC.swift
+//  DataCollector
+//
+//  Created by Aleksei Degtiarev on 28/02/2018.
+//  Copyright © 2018 Aleksei Degtiarev. All rights reserved.
+//
+
+import UIKit
+
+
+class SettingsTableVC: UITableViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+
+    
+
+}
